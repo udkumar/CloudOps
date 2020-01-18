4ef22dde6ec649bcd3b36301976499688919c5bd
udkumar <udkumar@hotmail.com>
2020-01-19 04:01:52 +0530

    generated serializer

--- app/serializers/aws_cloud_front_price_serializer.rb
+++ app/serializers/aws_cloud_front_price_serializer.rb
@@ -1,3 +1,6 @@
-class AwsCloudFrontPriceSerializer < ActiveModel::Serializer
-  attributes :id, :description, :begin_range, :end_range, :unit, :price_per_unit, :effective_date
+class AwsCloudFrontPriceSerializer
+  include FastJsonapi::ObjectSerializer
+  
+  set_type :effective_date
+  attributes :description, :begin_range, :end_range, :unit, :price_per_unit, :effective_date
 end
