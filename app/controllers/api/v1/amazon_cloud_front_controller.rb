7100b8f583ef0a9d61b3849644f1f1e180679981
udkumar <udkumar@hotmail.com>
2020-01-19 01:44:13 +0530

    updating code as per response required

--- app/controllers/api/v1/amazon_cloud_front_controller.rb
+++ app/controllers/api/v1/amazon_cloud_front_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::AmazonCloudFrontController < ApplicationController
   
 
   def region
-  	
+  	@aws = AwsCloudFrontPrice.all
+  	# AwsCloudFrontPriceSerializer.new(@aws).as_json
   end
 
   def sync_aws_cf_data
