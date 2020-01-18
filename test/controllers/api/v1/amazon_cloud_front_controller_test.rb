18c7f1b1ffbf36d07e3a257940da595ea0be83ff
udkumar <udkumar@hotmail.com>
2020-01-19 00:39:10 +0530

    endpint test controller

---
+++ test/controllers/api/v1/amazon_cloud_front_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class Api::V1::AmazonCloudFrontControllerTest < ActionDispatch::IntegrationTest
+  test "should get region" do
+    get api_v1_amazon_cloud_front_region_url
+    assert_response :success
+  end
+
+end
