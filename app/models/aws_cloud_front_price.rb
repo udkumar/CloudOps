eaf1aa1bfe7eb8906648e8f7ca77be1c62dfa8a9
udkumar <udkumar@hotmail.com>
2020-01-19 04:01:52 +0530

    added default aws service

--- app/models/aws_cloud_front_price.rb
+++ app/models/aws_cloud_front_price.rb
@@ -1,9 +1,12 @@
 class AwsCloudFrontPrice < ApplicationRecord
+	# attr_accessor :id, :description, :begin_range, :end_range, :unit, :price_per_unit, :effective_date
 	
-	before_save :default_values
+	before_save :service_source_default_name
   
   # Make it so that I can add pricing for more services when needed
-  def default_values
+  private
+
+  def service_source_default_name
     self.service_source ||= 'aws' if self.service_source.nil?
   end
 end
