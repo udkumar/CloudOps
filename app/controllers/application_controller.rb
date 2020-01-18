857325d94f79500b720d69d197d3d053d3b92079
udkumar <udkumar@hotmail.com>
2020-01-19 04:01:52 +0530

    global serializes object added for json response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
-	include ActionController::ImplicitRender
+
+	#won't need to do any change in your controllers except defining the serializer method
+	def render(options={})
+	  options[:json] = serializer.new(options[:json]).serialized_json
+	  super(options)
+	end
 end
