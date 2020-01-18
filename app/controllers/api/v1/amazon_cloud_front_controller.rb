56b30d4dc763c6ff133cdd5cc8359604b08dc5bb
udkumar <udkumar@hotmail.com>
2020-01-19 04:01:52 +0530

    added required action for filter price

--- app/controllers/api/v1/amazon_cloud_front_controller.rb
+++ app/controllers/api/v1/amazon_cloud_front_controller.rb
@@ -1,13 +1,26 @@
 class Api::V1::AmazonCloudFrontController < ApplicationController
   
 
-  def region
-  	@aws = AwsCloudFrontPrice.all
-  	# AwsCloudFrontPriceSerializer.new(@aws).as_json
+  def price_by_region
+  	# if params[:date]
+  	@cf_prices = AwsCloudFrontPrice.where("region_code > :value",  value: params[:id]).or(AwsCloudFrontPrice.where("effective_date > :filter_by_date",  filter_by_date: params[:date]))
+  	if @cf_prices
+  		render json: @cf_prices
+  	end
+  end
+
+  def filter_price_by_region
+  	render json: AwsCloudFrontPrice.find_by_effective_date(params[:date])
   end
 
   def sync_aws_cf_data
   	AwsServiceWorker.perform
+  end
+
+  private
+
+  def serializer
+    AwsCloudFrontPriceSerializer
   end
 
 end
