aeb8e47023e90cc9f75be82ddf1eba6aeddf16db
udkumar <udkumar@hotmail.com>
2020-01-20 02:55:20 +0530

    added testing data

---
+++ spec/factories/aws_cloud_front_price.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :aws_cloud_front_price do
+  	description { Faker::Lorem.word }
+    begin_range { Faker::Lorem.paragraph(2) }
+    end_range { Faker::Lorem.paragraph(2) }
+    unit { Faker::Lorem.paragraph(2) }
+    price_per_unit { Faker::Lorem.word }
+    region_name { Faker::Lorem.word }
+    service_source { Faker::Lorem.word }
+    region_code { Faker::Lorem.word }
+    rate_code { Faker::Invoice.reference }
+    effective_date { Faker::Date.in_date_period }
+  end
+end
