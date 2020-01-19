3416e2c376df87d9b8ad6da321fbe9d5ce0dbff1
udkumar <udkumar@hotmail.com>
2020-01-20 01:19:29 +0530

    removed sync action route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get 'amazon_cloud_front/region/:id' => 'amazon_cloud_front#price_by_region'
-      get 'amazon_cloud_front/sync_aws_cf_data'
     end
   end
 end
