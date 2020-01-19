20f6984066708027dad38137fd9748e3139b6ead
udkumar <udkumar@hotmail.com>
2020-01-19 19:36:28 +0530

    TODO

--- app/workers/aws_service_worker.rb
+++ app/workers/aws_service_worker.rb
@@ -1,16 +1,15 @@
-class AwsServiceWorker
+class AwsServiceWorker 
   include Sidekiq::Worker
   # include Sidekiq::Status::Worker
   sidekiq_options retry: false
 
-  def perform_in(*args)
+  def perform_in(*_args)
     cf_price_sync
   end
 
   private
 
- 	def cf_price_sync
- 		AwsServices::CloudFrontPriceSyncJson.new().get_cf_price
- 	end
-
+  def cf_price_sync
+    AwsServices::CloudFrontPriceSyncJson.new.get_cf_price
+  end
 end
