8c233b9ff2b4006c7da0713a41bb17045b44cea0
udkumar <udkumar@hotmail.com>
2020-01-20 02:55:20 +0530

    covered 1 test case

--- spec/controllers/api/v1/amazon_cloud_front_controller_spec.rb
+++ spec/controllers/api/v1/amazon_cloud_front_controller_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::AmazonCloudFrontController, type: :controller do
-  let(:valid_attributes) do
-    { description: '$0.00005001 per Lambda-GB-Second for  in US East (N. Virginia)', begin_range: '0', end_range: 'Inf', unit: 'Lambda-GB-Second', price_per_unit: 0.00005001, effective_date: '2019-12-01' }
-  end
 
-  let(:valid_session) { {} }
+  let!(:aws_cloud_front_prices) { create_list(:aws_cloud_front_price, 10) }
+  let(:region_code) { "us-east-1" }
+  
+  # Test suite for GET /api/v1/amazon_cloud_front/region/:id
+  describe 'GET /api/v1/amazon_cloud_front/region/:id' do
+    before { get '/api/v1/amazon_cloud_front/region/#{region_code}' }
 
-  describe 'GET #amazon_cloud_front/region/:id' do
-    it 'returns a success response' do
-      AwsCloudFrontPrice.where('region_code=? ', 'us-east-1' )
+    context 'when the record exists' do
+      it 'returns the amazon_cloud_front' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(region_code)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
       
-      get :price_by_region, params: { region_code: 'us-east-1' }
-      expect(response).to be_successful
-     
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match("{\"message\":\"Couldn't find AmazonCloudFront with 'id'=us-east-1\"}")
+      end
     end
   end
 end
