4d6879a271104c6b0b65ad87329474217839344f
udkumar <udkumar@hotmail.com>
2020-01-20 01:19:29 +0530

    remove API data sync action

--- app/controllers/api/v1/amazon_cloud_front_controller.rb
+++ app/controllers/api/v1/amazon_cloud_front_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AmazonCloudFrontController < ApplicationController
   def price_by_region
-    @cf_prices = AwsCloudFrontPrice.filter_by_region_date(params[:id], params[:date])
-    json_response(@cf_prices, '200')
+	    @cf_prices = AwsCloudFrontPrice.filter_by_region_date(params[:id], params[:date])
+	    json_response(@cf_prices)
   end
 
   private
