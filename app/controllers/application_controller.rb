044fe713fe383437cfb407b7d89fd9e51faa0f27
udkumar <udkumar@hotmail.com>
2020-01-19 19:36:28 +0530

    added exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
 
-	#won't need to do any change in your controllers except defining the serializer method
-	def render(options={})
-	  options[:json] = serializer.new(options[:json]).serialized_json
-	  super(options)
-	end
+  # won't need to do any change in your controllers except defining the serializer method
+  def render(options = {})
+    options[:json] = serializer.new(options[:json]).serialized_json
+    super(options)
+  end
 end
