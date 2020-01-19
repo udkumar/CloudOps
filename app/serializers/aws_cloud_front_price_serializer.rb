3e61c4239a96468cf8ac538e629cbaf8bb3c8b15
udkumar <udkumar@hotmail.com>
2020-01-19 19:36:28 +0530

    added response attribute - fastjson-api

--- app/serializers/aws_cloud_front_price_serializer.rb
+++ app/serializers/aws_cloud_front_price_serializer.rb
@@ -1,6 +1,6 @@
-class AwsCloudFrontPriceSerializer
+class AwsCloudFrontPriceSerializer 
   include FastJsonapi::ObjectSerializer
-  
+
   set_type :effective_date
   attributes :description, :begin_range, :end_range, :unit, :price_per_unit, :effective_date
 end
