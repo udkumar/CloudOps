d36b3224d738a2fd370fa602b38e814ffa2badb4
udkumar <udkumar@hotmail.com>
2020-01-19 19:37:17 +0530

    added test code

--- test/controllers/api/v1/amazon_cloud_front_controller_test.rb
+++ test/controllers/api/v1/amazon_cloud_front_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class Api::V1::AmazonCloudFrontControllerTest < ActionDispatch::IntegrationTest
-  test "should get region" do
+  test 'should get region' do
     get api_v1_amazon_cloud_front_region_url
     assert_response :success
   end
-
 end
