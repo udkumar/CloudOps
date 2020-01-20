afe41b80a3ff5b97832e8efdeea58f438998ee34
udkumar <udkumar@hotmail.com>
2020-01-20 19:06:06 +0530

    data type changed

--- db/migrate/20200117205104_create_aws_cloud_front_prices.rb
+++ db/migrate/20200117205104_create_aws_cloud_front_prices.rb
@@ -2,9 +2,9 @@ class CreateAwsCloudFrontPrices < ActiveRecord::Migration[6.0]
   def change
     create_table :aws_cloud_front_prices do |t|
       t.text :description
-      t.text :begin_range
-      t.text :end_range
-      t.text :unit
+      t.string :begin_range
+      t.string :end_range
+      t.string :unit
       t.float :price_per_unit
       t.date :effective_date
 
